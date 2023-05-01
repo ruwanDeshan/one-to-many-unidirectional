7882d20a764e91ed81bbef61bbc8ba4383ee43a6
ruwanDeshan <117300634+ruwanDeshan@users.noreply.github.com>
2023-05-01 10:29:03 +0530

    initialize

---
+++ src/AppInitializer.java
@@ -0,0 +1,36 @@
+import entity.Laptop;
+import entity.Student;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        try(Session session=HibernateUtil.getSessionFactory().openSession()){
+            Transaction transaction=session.beginTransaction();
+            //------------------------
+           /* Laptop laptop=new Laptop();
+            laptop.setBrand("Lenovo");
+
+            Student student=new Student();
+            student.setStudentName("kamal");
+
+            student.setLaptop(laptop);
+
+            session.persist(student);
+            //--------------------
+            transaction.commit();*/
+        }
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            Student student = session.get(Student.class, (long)1);
+            if (null!=student){
+                System.out.println(student.getStudentName());
+            }
+        }
+        try(Session session = HibernateUtil.getSessionFactory().openSession()){
+            Laptop laptop = session.get(Laptop.class, (long)1);
+            if (null!=laptop){
+                System.out.println(laptop.getStudent());
+            }
+        }
+    }
+}
